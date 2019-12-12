cdc28855b0d76b749dc1964630fe8084183f0109
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2019-12-12 18:14:24 +0900

    test

--- optuna/pruners/median.py
+++ optuna/pruners/median.py
@@ -33,7 +33,7 @@ class MedianPruner(PercentilePruner):
             will be postponed until a value is reported.
     """
 
-    def __init__(self, n_startup_trials=5, n_warmup_steps=0, interval_steps=1):
+    def __init__(self, n_startup_trials=7, n_warmup_steps=0, interval_steps=1):
         # type: (int, int, int) -> None
 
         super(MedianPruner, self).__init__(50.0, n_startup_trials, n_warmup_steps, interval_steps)
