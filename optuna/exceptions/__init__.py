56927b91036a019d9a49260a32782929042c2a92
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2020-10-04 14:03:32 +0900

    Exceptions to core

---
+++ optuna/exceptions/__init__.py
@@ -0,0 +1,6 @@
+from optuna.core.exceptions import CLIUsageError  # NOQA
+from optuna.core.exceptions import DuplicatedStudyError  # NOQA
+from optuna.core.exceptions import ExperimentalWarning  # NOQA
+from optuna.core.exceptions import OptunaError  # NOQA
+from optuna.core.exceptions import StorageInternalError  # NOQA
+from optuna.core.exceptions import TrialPruned  # NOQA
